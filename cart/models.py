5d48b7ced4192bcacf84b5a4d49b68b20072a422
Meeqat Suharwardy <meeqatsuharwardy@Meeqats-MacBook-Pro.local>
2023-06-21 13:15:14 +0100

    cart corrected

--- cart/models.py
+++ cart/models.py
@@ -6,7 +6,6 @@ from products.models import Product
 
 
 class Cart(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
 
--- cart/views.py
+++ cart/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Cart
 from .serializers import CartSerializer
-
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from .models import Product
 
 class CartListCreateView(generics.ListCreateAPIView):
     queryset = Cart.objects.all()
@@ -14,3 +16,13 @@ class CartListCreateView(generics.ListCreateAPIView):
 class CartRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
+
+
+def add_to_cart(request, product_id, quantity):
+    product = get_object_or_404(Product, id=product_id)
+
+    # Create the cart item
+    cart_item = Cart(product=product, quantity=quantity)
+    cart_item.save()
+
+    return JsonResponse({'message': 'Product added to cart.'})
