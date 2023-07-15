dcc5764d4d71136a692617a99231ee72a1ef4979
Meeqat Suharwardy <meeqatsuharwardy@Meeqats-MacBook-Pro.local>
2023-07-15 20:43:31 +0100

    main

--- orders/serializers.py
+++ orders/serializers.py
@@ -2,7 +2,7 @@
 from rest_framework import serializers
 from .models import Order, OrderItem
 from products.serializers import ProductSerializer
-from products.models import Product
+
 class OrderItemSerializer(serializers.ModelSerializer):
     product_name = serializers.CharField(source='product.name')
     product_image = serializers.CharField(source='product.image')
@@ -24,19 +24,6 @@ class OrderSerializer(serializers.ModelSerializer):
         order = Order.objects.create(**validated_data)
 
         for item_data in order_items_data:
-            product_data = item_data.pop('product')
-            product = Product.objects.create(**product_data)
-            OrderItem.objects.create(order=order, product=product, **item_data)
+            OrderItem.objects.create(order=order, **item_data)
 
         return order
-
-
-
-
-class OrderSerializer(serializers.ModelSerializer):
-    order_items = OrderItemSerializer(many=True, source='orderitem_set.all')
-
-    class Meta:
-        model = Order
-        fields = ['id', 'fullName', 'address', 'email', 'phoneNumber', 'postcode', 'total_price', 'is_successful', 'created_at', 'order_items']
-        read_only_fields = ['id', 'is_successful', 'created_at']
