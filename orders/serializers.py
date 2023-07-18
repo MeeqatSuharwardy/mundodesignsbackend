a63561de727b4035af8f4fc116e0a306ee05da13
Meeqat Suharwardy <meeqatsuharwardy@Meeqats-MacBook-Pro.local>
2023-07-18 22:14:00 +0100

    main

--- orders/serializers.py
+++ orders/serializers.py
@@ -8,17 +8,19 @@ class OrderItemSerializer(serializers.ModelSerializer):
         fields = ['product', 'quantity']
 
 class OrderSerializer(serializers.ModelSerializer):
-    items = OrderItemSerializer(many=True)
+    items = serializers.JSONField(write_only=True)
 
     class Meta:
         model = Order
         fields = ['id', 'fullName', 'session_id', 'address', 'postcode', 'email', 'phoneNumber', 'total_price', 'is_successful', 'created_at', 'items']
 
     def create(self, validated_data):
-        items_data = json.loads(validated_data.pop('items'))
+        items_data = validated_data.pop('items')
         order = Order.objects.create(**validated_data)
         for item_data in items_data:
             product_data = item_data.pop('product')
-            product = Product.objects.create(**product_data)
+            product_id = product_data.get('id')
+            product = Product.objects.get(id=product_id)
             OrderItem.objects.create(order=order, product=product, **item_data)
         return order
+
