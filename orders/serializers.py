70de98b05cc18a3a36e704eb672c917358952d08
Meeqat Suharwardy <meeqatsuharwardy@Meeqats-MacBook-Pro.local>
2023-07-21 19:46:35 +0100

    main

--- orders/serializers.py
+++ orders/serializers.py
@@ -7,7 +7,7 @@ class OrderItemSerializer(serializers.ModelSerializer):
         fields = ['product_name', 'quantity', 'price']
 
 class OrderSerializer(serializers.ModelSerializer):
-    items = OrderItemSerializer(many=True, write_only=True)
+    items = OrderItemSerializer(many=True, write_only=True, required=False)
 
     class Meta:
         model = Order
